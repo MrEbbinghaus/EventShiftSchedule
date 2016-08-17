36af8d9e171a7c355ecec0bb821ede49caf77bf8
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2016-08-17 23:07:07 +0200

    Make 'Time'-field editable in the admin section

--- admin.py
+++ admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Slot, Position, Party
+from .models import Slot, Position, Party, Time
 
 admin.site.register(Slot)
 admin.site.register(Position)
 admin.site.register(Party)
+admin.site.register(Time)
