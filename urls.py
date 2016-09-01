fcd01e617594bbe10140db495883cd098269effe
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2016-09-01 16:19:54 +0200

    Fix broken urls

--- urls.py
+++ urls.py
@@ -3,8 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.pss_landing, name='pss_landing'),
-    url(r'^pss/$', views.shift_schedule, name='shift_schedule'),
+    url(r'^$', views.shift_schedule, name='shift_schedule'),
     url(r'^event/post/enter/$', views.enter, name='enter'),
     url(r'^event/(?P<event_id>[0-9]+)', views.shift_schedule_event, name='shift_schedule_event'),
 ]
